145939d50094606656822772cab8ecdf5ec25885
Bora Sura <borasura135@gmail.com>
2025-03-20 17:56:05 +0530

    Make Auth URL Env variable

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,12 @@
 import { createAuthClient } from "better-auth/react"
+// export const authClient = createAuthClient({
+//     // baseURL: "http://localhost:3000" // the base url of your auth server https://na-pani.vercel.app/
+//     baseURL: "https://na-pani.vercel.app"
+// })
+
+
 export const authClient = createAuthClient({
-    // baseURL: "http://localhost:3000" // the base url of your auth server https://na-pani.vercel.app/
-    baseURL: "https://na-pani.vercel.app"
-})
+    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:3000" // Fallback to the default URL if the env variable is not set
+});
 
 // export const { signIn, signUp, useSession } = createAuthClient()
