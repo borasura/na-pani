44dc73b6bf3fa8ad0cd0d583c706afc34b840dd2
Bora Sura <borasura135@gmail.com>
2025-03-20 17:24:47 +0530

    fixing auth base url

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000" // the base url of your auth server 
+    // baseURL: "http://localhost:3000" // the base url of your auth server https://na-pani.vercel.app/
+    baseURL: "https://na-pani.vercel.app"
 })
 
 // export const { signIn, signUp, useSession } = createAuthClient()
